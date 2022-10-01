e7a4f1b96d36c8d1dac9514a77dc3633ad7d4504
xP9nda <crystaljava191@gmail.com>
2022-10-01 20:14:29 +0100

    Update main.rs
    - hide console window

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,24 @@ use discrakt::{
 };
 use std::{thread::sleep, time::Duration};
 
+// https://stackoverflow.com/questions/29763647/how-to-make-a-program-that-does-not-display-the-console-window
+fn hide_console_window() {
+    use std::ptr;
+    use winapi::um::wincon::GetConsoleWindow;
+    use winapi::um::winuser::{ShowWindow, SW_HIDE};
+
+    let window = unsafe {GetConsoleWindow()};
+    // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+    if window != ptr::null_mut() {
+        unsafe {
+            ShowWindow(window, SW_HIDE);
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    hide_console_window();
+
     let cfg = load_config();
     let mut discord = Discord::new(cfg.discord_token);
     let mut trakt = Trakt::new(cfg.trakt_client_id, cfg.trakt_username);
